30f036cf51b13ac55c46de568116a1b3afc1644f
Jeff Pollard <jeff.pollard@gmail.com>
2011-03-13 22:20:00 -0700

    More fleshed out Adapter and MySQL adapter

--- exodus/adapter.py
+++ exodus/adapter.py
@@ -1,5 +1,31 @@
+class InvalidAdapterOption(Exception):
+  pass
+
 class Base(object):
   """Base adapter upon which all other interfaces must inherit from"""
+  
+  def command(self, database, options={}):
+    """Returns a string of the command to run given the dictionary of options"""
+    raise NotImplementedError( "Adapters must implement their own version of command()" )
     
 class MySQL(Base):
   """MySQL migration runner adapter"""
+  
+  # Mapping of options passed in to the command() method to their command line name
+  options_to_args = {
+    "host": "--host",
+    "user": "--user",
+    "password": "--password"
+  }
+  
+  def __init__(self):
+    self.bin = "mysql"
+  
+  def command(self, database, options={}):
+    """Returns the MySQL command for the given set of options"""
+    try:
+      cmds = [ "%s=%s" % (self.options_to_args[k], v) for (k,v) in options.items() ]
+      cmds.insert(0, self.bin)
+      return " ".join(cmds) + " " + database
+    except KeyError as ke:
+      raise InvalidAdapterOption("Option '%s' not valid" % ke)
--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -3,8 +3,43 @@ import exodus
 
 class TestAdapter(unittest.TestCase):
   
-  def test_passes(self):
-    pass
+  def test_base_adapter_command_method_raises_exception(self):
+    self.assertRaises(NotImplementedError, (lambda: exodus.adapter.Base().command('test')))
+
+# TODO: Add a helper to verify commands are between "mysql" and "database"
+class TestMySQLAdapter(unittest.TestCase):
+    
+  def test_raises_exception_without_database(self):
+    command = lambda: exodus.adapter.MySQL().command()
+    self.assertRaises(TypeError, command)
+  
+  def test_command_has_mysql_bin(self):
+    self.assertRegexpMatches(exodus.adapter.MySQL().command('test'), "^mysql")
+    
+  def test_command_has_database_at_end(self):
+    self.assertRegexpMatches(exodus.adapter.MySQL().command('test'), "test$")
+    
+  def test_unsupported_option_raises_invalid_option_exception(self):
+    command = lambda: exodus.adapter.MySQL().command('test', {'foo':'bar'})
+    self.assertRaises(exodus.adapter.InvalidAdapterOption, command)
+  
+  def test_command_with_host_returns_correct_string(self):
+    cmd = exodus.adapter.MySQL().command('test', {'host':'example.com'})
+    self.assertRegexpMatches(cmd, "--host=example.com")
+    
+  def test_command_with_username_returns_correct_string(self):
+    cmd = exodus.adapter.MySQL().command('test', {'host':'example.com'})
+    self.assertRegexpMatches(cmd, "--host=example.com")
+  
+  def test_command_with_password_returns_correct_string(self):
+    cmd = exodus.adapter.MySQL().command('test', {'password':'1234'})
+    self.assertRegexpMatches(cmd, "--password=1234")
+    
+  def test_command_with_user_returns_correct_string(self):
+    cmd = exodus.adapter.MySQL().command('test', {'user':'joe'})
+    self.assertRegexpMatches(cmd, "--user=joe")
+    
+
   
   # Adapter methods
   # 
